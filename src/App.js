35848dd6d00cae8a130543ed1ab9fc6d8997d195
Alexey Shmelkov <ashmelkov@aligntech.com>
2019-05-25 12:01:14 +0300

    [lesson-15] inner styles

--- src/App.js
+++ src/App.js
@@ -1,16 +1,29 @@
 import React from 'react';
-import Header from './components/Header';
-import Main from './components/Main';
-import Footer from './components/Footer';
 
 
 function App(){
+  const date = new Date();
+  const hours = date.getHours();
+  let timeOfDay;
+  const styles = {
+    fontSize: '50px'
+  };
+
+  if (hours < 12) {
+    timeOfDay = "morning";
+    styles.color = '#04756F';
+  } else if (hours >= 12 && hours < 17) {
+    timeOfDay = "afternoon";
+    styles.color = '#2E0927'
+  } else {
+    timeOfDay = "night"
+    styles.color = '#D90000'
+  }
+
+  
+
   return (
-    <div>
-      <Header />
-      <Main />
-      <Footer />
-    </div>
+    <h1 style={styles}>Good {timeOfDay}!</h1>
   )
 };
 
